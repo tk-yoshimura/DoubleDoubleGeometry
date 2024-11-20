098730c313ea7104212fe00aa4c16a4019d43c3f
tk-yoshimura <yoshimura8192@gmail.com>
2024-11-21 03:11:53 +0900

    cleanup

--- DoubleDoubleGeometry/Geometry3D/Polygon3D.cs
+++ DoubleDoubleGeometry/Geometry3D/Polygon3D.cs
@@ -1,7 +1,6 @@
 ﻿using DoubleDouble;
 using DoubleDoubleComplex;
 using DoubleDoubleGeometry.Geometry2D;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
