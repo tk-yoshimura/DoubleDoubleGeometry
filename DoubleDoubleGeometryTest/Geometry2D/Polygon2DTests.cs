7f33501ce7349da628f3fb9476001f612abb2633
tk-yoshimura <yoshimura8192@gmail.com>
2024-11-26 02:05:22 +0900

    fix: test convex

--- DoubleDoubleGeometryTest/Geometry2D/Polygon2DTests.cs
+++ DoubleDoubleGeometryTest/Geometry2D/Polygon2DTests.cs
@@ -77,6 +77,7 @@ namespace DoubleDoubleGeometryTest.Geometry2D {
         public void IsConvexTest() {
             for (int n = 3; n <= 16; n++) {
                 Assert.IsTrue(Polygon2D.IsConvex(Polygon2D.Regular(n)));
+                Assert.IsTrue(Polygon2D.IsConvex(-Polygon2D.Regular(n)));
             }
 
             {
@@ -106,6 +107,28 @@ namespace DoubleDoubleGeometryTest.Geometry2D {
                     vertex[i] *= -0.5;
 
                     Assert.IsFalse(Polygon2D.IsConvex(new Polygon2D(vertex)));
+                }
+            }
+
+            for (int n = 5; n <= 16; n++) {
+                for (int i = 0; i < n; i++) {
+                    for (int j = i + 1; j < n; j++) {
+                        Vector2D[] vertex = [.. Polygon2D.Regular(n).Vertex];
+
+                        (vertex[i], vertex[j]) = (vertex[j], vertex[i]);
+
+                        Assert.IsFalse(Polygon2D.IsConvex(-new Polygon2D(vertex)));
+                    }
+                }
+            }
+
+            for (int n = 4; n <= 16; n++) {
+                for (int i = 0; i < n; i++) {
+                    Vector2D[] vertex = [.. Polygon2D.Regular(n).Vertex];
+
+                    vertex[i] *= -0.5;
+
+                    Assert.IsFalse(Polygon2D.IsConvex(-new Polygon2D(vertex)));
                 }
             }
         }
