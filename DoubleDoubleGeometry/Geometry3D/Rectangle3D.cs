81ab87b601b46f045910b5cfc9364f37781c250e
tk-yoshimura <yoshimura8192@gmail.com>
2024-11-30 02:28:30 +0900

    add: rectangle 3d

---
+++ DoubleDoubleGeometry/Geometry3D/Rectangle3D.cs
@@ -0,0 +1,213 @@
+﻿using DoubleDouble;
+using DoubleDoubleComplex;
+using DoubleDoubleGeometry.Geometry2D;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DoubleDoubleGeometry.Geometry3D {
+
+    [DebuggerDisplay("{ToString(),nq}")]
+    public class Rectangle3D : IGeometry<Rectangle3D, Vector3D>, IFormattable {
+        public readonly Vector3D Center;
+        public readonly Vector2D Scale;
+        public readonly Quaternion Rotation;
+
+        private Rectangle3D(Vector3D center, Vector2D scale, Quaternion rotation, int _) {
+            this.Center = center;
+            this.Scale = scale;
+            this.Rotation = rotation;
+        }
+
+        public Rectangle3D(Vector3D center, Vector2D scale, Vector3D normal)
+            : this(center, scale, Vector3D.Rot((0d, 0d, 1d), normal.Normal), 0) { }
+
+        public Rectangle3D(Vector3D center, Vector2D scale, Quaternion rotation) {
+            this.Center = center;
+            this.Scale = scale;
+            this.Rotation = rotation.Normal;
+        }
+
+        public Rectangle3D(Rectangle2D rectangle) {
+            this.Center = (Vector3D)rectangle.Center;
+            this.Scale = rectangle.Scale;
+            this.Rotation = Quaternion.One;
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private Polygon3D polygon = null;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public Polygon3D Polygon
+            => polygon ??= new Polygon3D(
+                new Polygon2D((-Scale.X, -Scale.Y), (Scale.X, -Scale.Y), (Scale.X, Scale.Y), (-Scale.X, Scale.Y)),
+                Center, Rotation
+            );
+
+        public ReadOnlyCollection<Vector3D> Vertex => Polygon.Vertex;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public Vector3D Normal => Rotation * new Vector3D(0d, 0d, 1d);
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public ddouble Width => ddouble.Abs(Scale.X) * 2d;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public ddouble Height => ddouble.Abs(Scale.Y) * 2d;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public Vector2D Size => (Width, Height);
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public ddouble LongSide => ddouble.Max(Width, Height);
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public ddouble ShortSide => ddouble.Min(Width, Height);
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public ddouble Aspect => ddouble.Abs(Scale.Y / Scale.X);
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public ddouble Area => Width * Height;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public ddouble Perimeter => 2d * (Width + Height);
+
+        public static Rectangle3D operator +(Rectangle3D g) {
+            return g;
+        }
+
+        public static Rectangle3D operator -(Rectangle3D g) {
+            return new(-g.Center, -g.Scale, g.Rotation, 0);
+        }
+
+        public static Rectangle3D operator +(Rectangle3D g, Vector3D v) {
+            return new(g.Center + v, g.Scale, g.Rotation, 0);
+        }
+
+        public static Rectangle3D operator +(Vector3D v, Rectangle3D g) {
+            return new(g.Center + v, g.Scale, g.Rotation, 0);
+        }
+
+        public static Rectangle3D operator -(Rectangle3D g, Vector3D v) {
+            return new(g.Center - v, g.Scale, g.Rotation, 0);
+        }
+
+        public static Rectangle3D operator -(Vector3D v, Rectangle3D g) {
+            return new(v - g.Center, -g.Scale, g.Rotation, 0);
+        }
+
+        public static Rectangle3D operator *(Quaternion q, Rectangle3D g) {
+            return new(q * g.Center, q.SquareNorm * g.Scale, q.Normal * g.Rotation, 0);
+        }
+
+        public static Rectangle3D operator *(Rectangle3D g, ddouble r) {
+            return new(g.Center * r, g.Scale * r, g.Rotation, 0);
+        }
+
+        public static Rectangle3D operator *(Rectangle3D g, double r) {
+            return new(g.Center * r, g.Scale * r, g.Rotation, 0);
+        }
+
+        public static Rectangle3D operator *(ddouble r, Rectangle3D g) {
+            return g * r;
+        }
+
+        public static Rectangle3D operator *(double r, Rectangle3D g) {
+            return g * r;
+        }
+
+        public static Rectangle3D operator /(Rectangle3D g, ddouble r) {
+            return new(g.Center / r, g.Scale / r, g.Rotation, 0);
+        }
+
+        public static Rectangle3D operator /(Rectangle3D g, double r) {
+            return new(g.Center / r, g.Scale / r, g.Rotation, 0);
+        }
+
+        public static bool operator ==(Rectangle3D g1, Rectangle3D g2) {
+            return (g1.Center == g2.Center) && (g1.Scale == g2.Scale) && (g1.Rotation == g2.Rotation);
+        }
+
+        public static bool operator !=(Rectangle3D g1, Rectangle3D g2) {
+            return !(g1 == g2);
+        }
+
+        public static implicit operator Rectangle3D((Vector3D center, Vector2D scale, Quaternion rotation) g) {
+            return new(g.center, g.scale, g.rotation);
+        }
+
+        public static implicit operator (Vector3D center, Vector2D scale, Quaternion rotation)(Rectangle3D g) {
+            return (g.Center, g.Scale, g.Rotation);
+        }
+
+        public void Deconstruct(out Vector3D center, out Vector2D scale, out Quaternion rotation)
+            => (center, scale, rotation) = (Center, Scale, Rotation);
+
+        public static implicit operator (Vector3D v0, Vector3D v1, Vector3D v2, Vector3D v3)(Rectangle3D g) {
+            return (g.Vertex[0], g.Vertex[1], g.Vertex[2], g.Vertex[3]);
+        }
+
+        public static implicit operator Polygon3D(Rectangle3D g) {
+            return g.Polygon;
+        }
+
+        public void Deconstruct(out Vector3D v0, out Vector3D v1, out Vector3D v2, out Vector3D v3)
+            => (v0, v1, v2, v3) = (Vertex[0], Vertex[1], Vertex[2], Vertex[3]);
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static Rectangle3D Invalid { get; } = new(Vector3D.Invalid, Vector2D.Invalid, Quaternion.NaN, 0);
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public static Rectangle3D Zero { get; } = new(Vector3D.Zero, Vector2D.Zero, Quaternion.Zero, 0);
+
+        public static bool IsNaN(Rectangle3D g) {
+            return Vector3D.IsNaN(g.Center) || Vector2D.IsNaN(g.Scale) || Quaternion.IsNaN(g.Rotation);
+        }
+
+        public static bool IsZero(Rectangle3D g) {
+            return Vector3D.IsZero(g.Center) && Vector2D.IsZero(g.Scale) && Quaternion.IsZero(g.Rotation);
+        }
+
+        public static bool IsFinite(Rectangle3D g) {
+            return Vector3D.IsFinite(g.Center) && Vector2D.IsFinite(g.Scale) && Quaternion.IsFinite(g.Rotation);
+        }
+
+        public static bool IsInfinity(Rectangle3D g) {
+            return !IsFinite(g);
+        }
+
+        public static bool IsValid(Rectangle3D g) {
+            return IsFinite(g);
+        }
+
+        public override string ToString() {
+            return $"center={Center}, scale={Scale}, rotation={Rotation}";
+        }
+
+        public string ToString([AllowNull] string format, [AllowNull] IFormatProvider formatProvider) {
+            if (string.IsNullOrWhiteSpace(format)) {
+                return ToString();
+            }
+
+            return $"center={Center.ToString(format)}, scale=({Scale.ToString(format)}, rotation={Rotation.ToString(format)}";
+        }
+
+        public string ToString(string format) {
+            return ToString(format, null);
+        }
+
+        public override bool Equals(object obj) {
+            return ReferenceEquals(this, obj) || (obj is not null && obj is Rectangle3D g && g == this);
+        }
+
+        public bool Equals(Rectangle3D other) {
+            return ReferenceEquals(this, other) || (other is not null && other == this);
+        }
+
+        public override int GetHashCode() {
+            return Center.GetHashCode() ^ Scale.GetHashCode() ^ Rotation.GetHashCode();
+        }
+    }
+}
---
+++ DoubleDoubleGeometryTest/Geometry3D/Rectangle3DTests.cs
@@ -0,0 +1,103 @@
+﻿using DoubleDouble;
+using DoubleDoubleComplex;
+using DoubleDoubleGeometry.Geometry3D;
+using PrecisionTestTools;
+
+namespace DoubleDoubleGeometryTest.Geometry3D {
+    [TestClass()]
+    public class Rectangle3DTests {
+        [TestMethod()]
+        public void Rectangle3DTest() {
+            Rectangle3D rectangle = new((1, 2, 7), (4, 3), Vector3D.Rot((0, 0, 1), (2, 3, 4)));
+
+            Vector3DAssert.AreEqual((1, 2, 7), rectangle.Center, 1e-30);
+            Vector3DAssert.AreEqual(new Vector3D(2, 3, 4).Normal, rectangle.Normal, 1e-30);
+            Assert.AreEqual(4d, rectangle.Scale.X);
+            Assert.AreEqual(3d, rectangle.Scale.Y);
+            Assert.AreEqual(Vector3D.Rot((0, 0, 1), (2, 3, 4)).Normal, rectangle.Rotation);
+
+            PrecisionAssert.AreEqual(8 * 6, rectangle.Area);
+            PrecisionAssert.AreEqual((8 + 6) * 2, rectangle.Perimeter, 1e-30);
+        }
+
+        [TestMethod]
+        public void EqualTest() {
+            Quaternion rot = Quaternion.FromAxisAngle(new Vector3D(2, 3, 4).Normal, 5);
+
+            Rectangle3D rectangle1 = new((1, 2, 7), (4, 3), rot);
+            Rectangle3D rectangle2 = new((1, 2, 7), (4, 3), rot);
+            Rectangle3D rectangle3 = new((1, 2, 7), (4, 4), rot);
+            Rectangle3D rectangle4 = new((1, 2, 7), (4, 4), rot);
+
+            Assert.AreEqual(rectangle1, rectangle2);
+            Assert.AreNotEqual(rectangle1, rectangle3);
+
+            Assert.IsTrue(rectangle1 == rectangle2);
+            Assert.IsTrue(rectangle1 != rectangle3);
+            Assert.IsTrue(rectangle1 != rectangle4);
+        }
+
+        [TestMethod()]
+        public void OperatorTest() {
+            Quaternion rot = Quaternion.FromAxisAngle(new Vector3D(2, 3, 4).Normal, 5);
+
+            Assert.AreEqual(new Rectangle3D((1, 2, 7), (4, 3), rot), +(new Rectangle3D((1, 2, 7), (4, 3), rot)));
+            Assert.AreEqual(new Rectangle3D((-1, -2, -7), (-4, -3), rot), -(new Rectangle3D((1, 2, 7), (4, 3), rot)));
+            Assert.AreEqual(new Rectangle3D((2, 6, 12), (4, 3), rot), new Rectangle3D((1, 2, 7), (4, 3), rot) + (1, 4, 5));
+            Assert.AreEqual(new Rectangle3D((0, -2, 2), (4, 3), rot), new Rectangle3D((1, 2, 7), (4, 3), rot) - (1, 4, 5));
+            Assert.AreEqual(new Rectangle3D((2, 6, 12), (4, 3), rot), (1, 4, 5) + new Rectangle3D((1, 2, 7), (4, 3), rot));
+            Assert.AreEqual(new Rectangle3D((0, 2, -2), (-4, -3), rot), (1, 4, 5) - new Rectangle3D((1, 2, 7), (4, 3), rot));
+            Assert.AreEqual(new Rectangle3D((2, 4, 14), (8, 6), rot), new Rectangle3D((1, 2, 7), (4, 3), rot) * (ddouble)2);
+            Assert.AreEqual(new Rectangle3D((2, 4, 14), (8, 6), rot), new Rectangle3D((1, 2, 7), (4, 3), rot) * (double)2);
+            Assert.AreEqual(new Rectangle3D((2, 4, 14), (8, 6), rot), (ddouble)2 * new Rectangle3D((1, 2, 7), (4, 3), rot));
+            Assert.AreEqual(new Rectangle3D((2, 4, 14), (8, 6), rot), (double)2 * new Rectangle3D((1, 2, 7), (4, 3), rot));
+            Assert.AreEqual(new Rectangle3D((0.5, 1, 3.5), (2, 1.5), rot), new Rectangle3D((1, 2, 7), (4, 3), rot) / (ddouble)2);
+            Assert.AreEqual(new Rectangle3D((0.5, 1, 3.5), (2, 1.5), rot), new Rectangle3D((1, 2, 7), (4, 3), rot) / (double)2);
+        }
+
+        [TestMethod()]
+        public void PointTest() {
+            Rectangle3D rectangle1 = new(Vector3D.Zero, (4, 3), (0, 0, 1));
+            Rectangle3D rectangle2 = new Rectangle3D(Vector3D.Zero, (4, 3), (0, 0, 1)) * 2;
+            Rectangle3D rectangle3 = new Rectangle3D(Vector3D.Zero, (4, 3), (0, 0, 1)) * -2;
+            Rectangle3D rectangle4 = new((2, 3, 4), (4, 3), (0, 0, 1));
+
+            Quaternion q = (1, 2, 3, 4);
+
+            Rectangle3D rectangle5 = q * new Rectangle3D((2, 3, 4), (20, 15), (0, 0, 1));
+
+            Vector3DAssert.AreEqual((-4, -3, 0), rectangle1.Vertex[0], 1e-30);
+            Vector3DAssert.AreEqual((4, -3, 0), rectangle1.Vertex[1], 1e-30);
+            Vector3DAssert.AreEqual((4, 3, 0), rectangle1.Vertex[2], 1e-30);
+            Vector3DAssert.AreEqual((-4, 3, 0), rectangle1.Vertex[3], 1e-30);
+
+            Vector3DAssert.AreEqual(rectangle1.Vertex[0] * 2, rectangle2.Vertex[0], 1e-30);
+            Vector3DAssert.AreEqual(rectangle1.Vertex[1] * 2, rectangle2.Vertex[1], 1e-30);
+            Vector3DAssert.AreEqual(rectangle1.Vertex[2] * 2, rectangle2.Vertex[2], 1e-30);
+            Vector3DAssert.AreEqual(rectangle1.Vertex[3] * 2, rectangle2.Vertex[3], 1e-30);
+
+            Vector3DAssert.AreEqual(rectangle1.Vertex[0] * -2, rectangle3.Vertex[0], 1e-30);
+            Vector3DAssert.AreEqual(rectangle1.Vertex[1] * -2, rectangle3.Vertex[1], 1e-30);
+            Vector3DAssert.AreEqual(rectangle1.Vertex[2] * -2, rectangle3.Vertex[2], 1e-30);
+            Vector3DAssert.AreEqual(rectangle1.Vertex[3] * -2, rectangle3.Vertex[3], 1e-30);
+
+            Vector3DAssert.AreEqual(rectangle1.Vertex[0] + (2, 3, 4), rectangle4.Vertex[0], 1e-30);
+            Vector3DAssert.AreEqual(rectangle1.Vertex[1] + (2, 3, 4), rectangle4.Vertex[1], 1e-30);
+            Vector3DAssert.AreEqual(rectangle1.Vertex[2] + (2, 3, 4), rectangle4.Vertex[2], 1e-30);
+            Vector3DAssert.AreEqual(rectangle1.Vertex[3] + (2, 3, 4), rectangle4.Vertex[3], 1e-30);
+
+            Vector3DAssert.AreEqual(q * (rectangle1.Vertex[0] * 5 + (2, 3, 4)), rectangle5.Vertex[0], 2e-29);
+            Vector3DAssert.AreEqual(q * (rectangle1.Vertex[1] * 5 + (2, 3, 4)), rectangle5.Vertex[1], 2e-29);
+            Vector3DAssert.AreEqual(q * (rectangle1.Vertex[2] * 5 + (2, 3, 4)), rectangle5.Vertex[2], 2e-29);
+            Vector3DAssert.AreEqual(q * (rectangle1.Vertex[3] * 5 + (2, 3, 4)), rectangle5.Vertex[3], 2e-29);
+        }
+
+        [TestMethod()]
+        public void ValidTest() {
+            Quaternion rot = Quaternion.FromAxisAngle(new Vector3D(2, 3, 4).Normal, 5);
+
+            Assert.IsTrue(Rectangle3D.IsValid(new Rectangle3D((1, 2, 7), (4, 3), rot)));
+            Assert.IsFalse(Rectangle3D.IsValid(Rectangle3D.Invalid));
+        }
+    }
+}
